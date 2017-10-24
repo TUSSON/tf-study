fe5894bcb0dec8fdacca1f030c7428683f3dc17d
tusson <dusong@rock-chips.com>
2017-10-24 11:57:44 +0800

    estimator predict too slow, change to basic api.
    Signed-off-by: tusson <dusong@rock-chips.com>

--- DQN/flappybird/deep_q_net.py
+++ DQN/flappybird/deep_q_net.py
@@ -8,11 +8,9 @@ MINI_BATCH_SIZE = 32
 GAMMA = 0.99
 OBSERVE = 1000
 
-def cnn_model_fn(features, labels, mode):
+def inference():
     """Model function for CNN."""
-    # Input Layer
-    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-    input_layer = tf.reshape(features["x"], [-1, 80, 80, 4])
+    s = tf.placeholder(tf.float32, shape=(None, 80, 80, 4))
 
     # Convolutional Layer #1
     # Computes 32 features using a 8x8 filter with ReLU activation.
@@ -20,7 +18,7 @@ def cnn_model_fn(features, labels, mode):
     # Input Tensor Shape: [batch_size, 80, 80, 4]
     # Output Tensor Shape: [batch_size, 20, 20, 32]
     conv1 = tf.layers.conv2d(
-        inputs=input_layer,
+        inputs=s,
         filters=32,
         kernel_size=[8, 8],
         strides=(4, 4),
@@ -64,67 +62,44 @@ def cnn_model_fn(features, labels, mode):
     # Output Tensor Shape: [batch_size, 2]
     logits = tf.layers.dense(inputs=dense, units=2)
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=logits)
+    return s, logits
 
-    action = tf.reshape(features["a"], [-1,2])
-    qa = tf.reduce_sum(logits * action, axis=1)
+def netloss(logits):
+    a = tf.placeholder(tf.float32, shape=(None, 2))
+    q = tf.placeholder(tf.float32, shape=(None))
+    qa = tf.reduce_sum(logits * a, axis=1)
     # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.reduce_mean(tf.square(labels - qa))
+    loss = tf.reduce_mean(tf.square(q - qa))
+    return a, q, loss
 
-    # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer()
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(labels=labels, predictions=qa)}
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-def get_predict_input_fn(datasets):
-    return tf.estimator.inputs.numpy_input_fn(
-            x={"x": datasets},
-            num_epochs=1,
-            shuffle=False)
-
-def get_train_input_fn(datasets, actions, labels, num_epochs=1, shuffle=False):
-    return tf.estimator.inputs.numpy_input_fn(
-            x={"x": datasets, "a": actions},
-            y=labels,
-            num_epochs=num_epochs,
-            shuffle=shuffle)
+def training(loss):
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return train_op
 
 def trainDeepQNet(frameStep):
+    s, logits = inference()
+    a, q, loss = netloss(logits)
+    train_op = training(loss)
 
-    cnn_net = tf.estimator.Estimator(model_fn=cnn_model_fn,
-                                     model_dir="./saved_model")  
-    
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    sess = tf.Session()
+    sess.run(init)
+
     status, reward, terminal =  frameStep(0)
     status = np.stack((status, status, status, status), axis=2)
     status = status.astype(np.float32)
 
-    epsilon = 0.05
+    epsilon = 0.5
     t = 0
 
     D = []
 
     while True:
-        input_fn = get_predict_input_fn(status[np.newaxis,:])
-        try:
-            predictions = cnn_net.predict(input_fn=input_fn)
-            q_value = [p[1] for p in enumerate(predictions)][0]
-        except ValueError:
-            print('-------first train model-------')
-            actions=np.zeros((1, 2), dtype=np.float32)
-            labels=np.zeros((1,1), dtype=np.float32)
-            train_input_fn = get_train_input_fn(status[np.newaxis,:], actions, labels)
-            cnn_net.train(input_fn=train_input_fn, steps=1)
-            q_value = labels
+        q_value = sess.run(logits, feed_dict={s: [status]})[0]
 
         #select an action
         #    with probability epsilon select a random action
@@ -134,6 +109,10 @@ def trainDeepQNet(frameStep):
             print('--------random action----------')
         else:
             action = q_value.argmax()
+
+        if epsilon > 0.001:
+            if t % 1000 == 0:
+                epsilon = epsilon * 0.9
             
         #carry out action
         new_status, reward, terminal = frameStep(action)
@@ -141,10 +120,10 @@ def trainDeepQNet(frameStep):
         new_status = new_status[:,:,np.newaxis]
         new_status = np.append(new_status, status[:,:,:3], axis=2)
 
-        a = np.zeros(2, dtype=np.float32)
-        a[action] = 1
+        a_t = np.zeros([2], dtype=np.float32)
+        a_t[action] = 1
         #store experience <status, action, reward, new_status> in replay memory D
-        D.append((status, a, reward, new_status, terminal))
+        D.append((status, a_t, reward, new_status, terminal))
 
         if len(D) > MAX_REPLAY_MEMORY:
             D.pop(0)
@@ -152,32 +131,32 @@ def trainDeepQNet(frameStep):
         if t > OBSERVE:
             #sample random transitions from replay memory D
             exp_batch = random.sample(D, MINI_BATCH_SIZE)
-            s_batch = np.asarray([exp[0] for exp in exp_batch])
-            a_batch = np.asarray([exp[1] for exp in exp_batch])
-            r_batch = np.asarray([exp[2] for exp in exp_batch], dtype=np.float32)
-            news_batch = np.asarray([exp[3] for exp in exp_batch])
-            t_batch = np.asarray([exp[4] for exp in exp_batch])
+            s_batch = [exp[0] for exp in exp_batch]
+            a_batch = [exp[1] for exp in exp_batch]
+            r_batch = [exp[2] for exp in exp_batch]
+            news_batch = [exp[3] for exp in exp_batch]
+            t_batch = [exp[4] for exp in exp_batch]
 
             #calculate target for each minibatch transition
-            input_fn = get_predict_input_fn(news_batch)
-            newq_batch = cnn_net.predict(input_fn=input_fn)
-            newq_batch = np.asarray([p[1] for p in enumerate(newq_batch)])
-            q_batch = np.empty(len(newq_batch), dtype=np.float32)
+            newq_batch = sess.run(logits, feed_dict={s: news_batch})
+            q_batch = []
 
             for i in range(len(newq_batch)):
                 terminal = t_batch[i]
                 reward = r_batch[i]
                 #if new_status is terminal state then tt=rr
                 if terminal:
-                    q_batch[i] = reward
+                    q_batch.append(reward)
                 #otherwise tt=rr + y*max(Q(new_status))
                 else:
-                    q_batch[i] = reward + GAMMA*np.max(newq_batch[i])
-            train_input_fn = get_train_input_fn(s_batch, a_batch, q_batch)
-            cnn_net.train(input_fn=train_input_fn, steps=1)
+                    q_batch.append(reward + GAMMA*np.max(newq_batch[i]))
+            sess.run(train_op, feed_dict={s: s_batch, a:a_batch, q:q_batch})
 
         status = new_status 
         t += 1
+
+        if t % 1000 == 0:
+            saver.save(sess, 'saved_model/bird-dqn', global_step = t)
 
         print(t, 'epsilon:', epsilon, 'action:',
               action, 'reward:', reward,
